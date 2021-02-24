08ae5f3f59c1edc84dd73d82b5ca0bcd430355a5
Lewis Sneddon <>
2021-02-24 20:29:51 +0000

    extracted projects as array of objects to be passed as props to Project component

---
+++ src/Components/Projects/Project.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Project = (props) => {
+  return (
+    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+      <div class="flex-grow-1">
+        <h3 class="mb-0">{props.title}</h3>
+        <ul class="list-inline dev-icons">
+          <li class="list-inline-item">
+            <i class="fab fa-react"></i>
+          </li>
+        </ul>
+        <p>{props.description}</p>
+      </div>
+      <a
+        href={props.launchUrl}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <button class="btn-primary btn-lg code-button">
+          <i class="fa fa-rocket"></i> Launch
+        </button>
+      </a>
+      &ensp;
+      <a
+        href={props.codeUrl}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <button class="btn-primary btn-lg code-button">
+          <i class="fab fa-github"></i> Code
+        </button>
+      </a>
+    </div>
+  );
+};
+
+export default Project;
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,10 +1,53 @@
 import React from "react";
+import Project from './Project';
+
+const projectsData = [
+  {
+    title: "Bake Buddy",
+    description: "A conversion app for common baking ingredients.",
+    codeUrl: "https://github.com/lsneddon-cmd/Bake_Buddy_React",
+    launchUrl: "https://bake-buddy.netlify.app/",
+    techStack: ["react"],
+  },
+  {
+    title: "Matrix Calculator",
+    description: `A special calculator that uses the command line interface to accept matrix input and carry out a variety of matrix related calculations This program supports; addition, multiplication, scalar multiplication, transposition, determinant and inverse.`,
+    codeUrl:
+      "https://github.com/lsneddon-cmd/MatrixCalculator/tree/master/src/com/lsneddon/matrixCalculator",
+    launchUrl: null,
+    techStack: ["java"],
+  },
+  {
+    title: "Battleships",
+    description: "A Battleship game for terminal.",
+    codeUrl:
+      "https://github.com/lsneddon-cmd/Battleship/tree/master/Battleship/task/src/battleship",
+    launchUrl: null,
+    techStack: ["java"],
+  },
+  {
+    title: "Simple Banking System",
+    description: `A terminal banking app to generate new accounts with unique account numbers; validate access with unique PIN; and validate card number with the Luhn algorithm`,
+    codeUrl:
+      "https://github.com/lsneddon-cmd/Simple_Banking_System/tree/master/Simple%20Banking%20System/task/src/banking",
+    launchUrl: null,
+    techStack: ["java"],
+  },
+];
 
 const Projects = () => {
   return (
     <section class="resume-section" id="projects">
       <div class="resume-section-content">
         <h2 class="mb-5">Projects</h2>
+
+        <Project 
+          title={projectsData[0].title} 
+          description={projectsData[0].description}
+          codeUrl={projectsData[0].codeUrl}
+          launchUrl={projectsData[0].launchUrl}
+          techStack={projectsData[0].techStack[0]}
+        />
 
         <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
           <div class="flex-grow-1">
@@ -78,26 +121,10 @@ const Projects = () => {
           </a>
         </div>
 
-        <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
-          <div class="flex-grow-1">
-            <h3 class="mb-0">Bake buddy</h3>
-            <ul class="list-inline dev-icons">
-              <li class="list-inline-item">
-                <i class="fab fa-react"></i>
-              </li>
-            </ul>
-            <p>A conversion app for common baking ingredients.</p>
-          </div>
-          <a
-            href="https://github.com/lsneddon-cmd/Bake_Buddy_React"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <button class="btn-primary btn-lg code-button">
-              <i class="fab fa-github"></i> Code
-            </button>
-          </a>
-        </div>
+        
+
+        
+
       </div>
     </section>
   );
