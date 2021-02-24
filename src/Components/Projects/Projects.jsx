722c09f8b10e8b572c2a34a246d6b6d91c9d4448
Lewis Sneddon <>
2021-02-24 21:48:30 +0000

    mapping through projects

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -44,45 +44,18 @@ const Projects = () => {
     <section class="resume-section" id="projects">
       <div class="resume-section-content">
         <h2 class="mb-5">Projects</h2>
-
-        <Project
-          title={projectsData[0].title}
-          description={projectsData[0].description}
-          codeUrl={projectsData[0].codeUrl}
-          launchUrl={projectsData[0].launchUrl}
-          techStack={projectsData[0].techStack[0]}
-          launchable={projectsData[0].launchable}
+        {projectsData.map(i => {
+          return (
+            <Project
+          title={i.title}
+          description={i.description}
+          codeUrl={i.codeUrl}
+          launchUrl={i.launchUrl}
+          techStack={i.techStack[0]}
+          launchable={i.launchable}
         />
-
-<Project
-          title={projectsData[1].title}
-          description={projectsData[1].description}
-          codeUrl={projectsData[1].codeUrl}
-          launchUrl={projectsData[1].launchUrl}
-          techStack={projectsData[1].techStack[0]}
-          launchable={projectsData[1].launchable}
-        />
-
-<Project
-          title={projectsData[2].title}
-          description={projectsData[2].description}
-          codeUrl={projectsData[2].codeUrl}
-          launchUrl={projectsData[2].launchUrl}
-          techStack={projectsData[2].techStack[0]}
-          launchable={projectsData[2].launchable}
-        />
-
-<Project
-          title={projectsData[3].title}
-          description={projectsData[3].description}
-          codeUrl={projectsData[3].codeUrl}
-          launchUrl={projectsData[3].launchUrl}
-          techStack={projectsData[3].techStack[0]}
-          launchable={projectsData[3].launchable}
-        />
-     
-
-      
+          )
+        })}
       </div>
     </section>
   );
