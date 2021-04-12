abf8200bd6db873de7ee3a0e783ccec447f12841
codeslewis <lewis.sneddon@icloud.com>
2021-04-12 19:40:16 +0100

    extracted changeable information to objects in data directory

---
+++ src/data/projectInfo.js
@@ -0,0 +1,45 @@
+export const projectsData = [
+  {
+    title: "Minesweeper",
+    description: "Command Line Minesweeper game",
+    codeUrl:
+      "https://github.com/codeslewis/Minesweeper/tree/master/Minesweeper/task/src/minesweeper",
+    launchable: false,
+    launchUrl: null,
+    techStack: ["kotlin"],
+  },
+  {
+    title: "Bake Buddy",
+    description: "A conversion app for common baking ingredients.",
+    codeUrl: "https://github.com/codeslewis/Baking_Conversions",
+    launchable: true,
+    launchUrl: "https://bake-buddy.netlify.app/",
+    techStack: ["react", "js"],
+  },
+  {
+    title: "Recipes",
+    description: `Andriod recipe app. Utilises RecyclerView to display a list of recipes.`,
+    codeUrl: "https://github.com/codeslewis/Recipes/tree/master/app/src",
+    launchable: false,
+    launchUrl: null,
+    techStack: ["android", "kotlin"],
+  },
+  {
+    title: "Battleships",
+    description: "A Battleship game for terminal.",
+    codeUrl:
+      "https://github.com/codeslewis/Battleship/tree/master/Battleship/task/src/battleship",
+    launchable: false,
+    launchUrl: null,
+    techStack: ["java"],
+  },
+  {
+    title: "Bake Buddy Android",
+    description: `A baking ingredient conversion app. This Android app converts Imperial and US Legal baking ingredient measurements into the metric system.`,
+    codeUrl:
+      "https://github.com/codeslewis/Bake_Buddy_Android/tree/master/app/src",
+    launchable: false,
+    launchUrl: null,
+    techStack: ["android", "java"],
+  },
+];
