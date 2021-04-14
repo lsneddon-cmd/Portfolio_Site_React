33b273f05a57cd029121669895c535413bd22c86
codeslewis <lewis.sneddon@icloud.com>
2021-04-14 07:13:55 +0100

    split projectd into tabs

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,27 +1,75 @@
 import React from "react";
 import Project from "./Project";
-import { projectsData } from "../../data/projectInfo";
+import { projectsData, webProjects, mobileProjects, terminalProjects } from "../../data/projectInfo";
+import { Tabs, Tab } from "react-bootstrap";
 
 const Projects = () => {
   return (
     <section className="resume-section" id="projects">
       <div className="resume-section-content">
         <h2 className="mb-5">Projects</h2>
-        <div className="card-deck">
-          {projectsData.map((i) => {
-            return (
-              <Project
-                title={i.title}
-                description={i.description}
-                codeUrl={i.codeUrl}
-                launchUrl={i.launchUrl}
-                techStack={i.techStack}
-                launchable={i.launchable}
-                key={i.title}
-              />
-            );
-          })}
-        </div>
+
+        <Tabs defaultActiveKey="Command Line">
+          <Tab eventKey="Command Line" title="Command Line">
+            <div className="card-deck">
+              {terminalProjects.map((i) => {
+                return (
+                  <Project
+                    title={i.title}
+                    description={i.description}
+                    codeUrl={i.codeUrl}
+                    launchUrl={i.launchUrl}
+                    techStack={i.techStack}
+                    launchable={i.launchable}
+                    key={i.title}
+                  />
+                );
+              })}
+            </div>
+          </Tab>
+          <Tab eventKey="Web" title="Web">
+            <div className="card-deck">
+              {webProjects.map((i) => {
+                return (
+                  <Project
+                    title={i.title}
+                    description={i.description}
+                    codeUrl={i.codeUrl}
+                    launchUrl={i.launchUrl}
+                    techStack={i.techStack}
+                    launchable={i.launchable}
+                    key={i.title}
+                  />
+                );
+              })}
+            </div>
+          </Tab>
+          <Tab eventKey="Mobile" title="Mobile" >
+            <div className="card-deck">
+              {mobileProjects.map((i) => {
+                return (
+                  <Project
+                    title={i.title}
+                    description={i.description}
+                    codeUrl={i.codeUrl}
+                    launchUrl={i.launchUrl}
+                    techStack={i.techStack}
+                    launchable={i.launchable}
+                    key={i.title}
+                  />
+                );
+              })}
+            </div>
+          </Tab>
+          <Tab eventKey="Desktop" title="Desktop" >
+
+          </Tab>
+        </Tabs>
+
+
+
+
+
       </div>
     </section>
   );
--- src/data/projectInfo.js
+++ src/data/projectInfo.js
@@ -43,3 +43,47 @@ export const projectsData = [
     techStack: ["android", "java"],
   },
 ];
+
+export const webProjects = [
+  {
+    title: "Bake Buddy",
+    description: "A conversion app for common baking ingredients.",
+    codeUrl: "https://github.com/codeslewis/Baking_Conversions",
+    launchable: true,
+    launchUrl: "https://bake-buddy.netlify.app/",
+    techStack: ["react", "js"],
+  },
+];
+
+export const terminalProjects = [
+  {
+    title: "Minesweeper",
+    description: "Command Line Minesweeper game",
+    codeUrl:
+      "https://github.com/codeslewis/Minesweeper/tree/master/Minesweeper/task/src/minesweeper",
+    launchable: false,
+    launchUrl: null,
+    techStack: ["kotlin"],
+  },
+  {
+    title: "Battleships",
+    description: "A Battleship game for terminal.",
+    codeUrl:
+      "https://github.com/codeslewis/Battleship/tree/master/Battleship/task/src/battleship",
+    launchable: false,
+    launchUrl: null,
+    techStack: ["java"],
+  },
+];
+
+export const mobileProjects = [
+  {
+    title: "Bake Buddy (Android)",
+    description: `A baking ingredient conversion app. This Android app converts Imperial and US Legal baking ingredient measurements into the metric system.`,
+    codeUrl:
+      "https://github.com/codeslewis/Bake_Buddy_Android/tree/master/app/src",
+    launchable: false,
+    launchUrl: null,
+    techStack: ["android", "java"],
+  },
+];
