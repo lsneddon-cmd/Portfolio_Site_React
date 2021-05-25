3861aad7f9e2e56bd96487ffd9315f2a8581b918
codeslewis <lewis.sneddon@icloud.com>
2021-05-25 16:10:19 +0100

    cert badge

--- src/Components/Certifications/Certifications.jsx
+++ src/Components/Certifications/Certifications.jsx
@@ -1,16 +1,25 @@
 import React from "react";
+import "./Certifications.css";
 
 const Certifications = () => {
   return (
     <section className="resume-section" id="certifications">
       <div className="resume-section-content">
         <h2 className="mb-5">Certifications</h2>
-        <div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="f52a729d-d239-4178-9183-9e022731d144" data-share-badge-host="https://www.credly.com">
-          </div>
-          
+        <ul className="list-inline dev-icons">
+          <li className="list-inline-item">
+          <a href="https://www.credly.com/badges/f52a729d-d239-4178-9183-9e022731d144/public_url"
+            target="_blank"
+          >
+            <img className="cert-badges" src="/microsoft-certified-azure-fundamentals.png" alt="Microsoft Azure Fundamentals" />
+            </a>
+          </li>
+        </ul>
+                    
       </div>
     </section>
   );
 };
+
 
 export default Certifications;
