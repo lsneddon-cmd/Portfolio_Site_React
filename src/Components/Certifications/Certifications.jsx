a4753d22b2d373db6b1e9b06d58c38548d653f60
codeslewis <lewis.sneddon@icloud.com>
2021-05-25 16:12:54 +0100

    new target window for cert verification

--- src/Components/Certifications/Certifications.jsx
+++ src/Components/Certifications/Certifications.jsx
@@ -10,6 +10,7 @@ const Certifications = () => {
           <li className="list-inline-item">
           <a href="https://www.credly.com/badges/f52a729d-d239-4178-9183-9e022731d144/public_url"
             target="_blank"
+            rel="noreferrer"
           >
             <img className="cert-badges" src="/microsoft-certified-azure-fundamentals.png" alt="Microsoft Azure Fundamentals" />
             </a>
