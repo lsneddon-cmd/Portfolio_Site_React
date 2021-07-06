3a0e8f545a764c21f00fd9c2568879d7f786db45
Lewis Sneddon <ec2-user@ip-172-31-87-37.ec2.internal>
2021-07-06 07:36:57 +0000

    data from static

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,20 +1,12 @@
 import React from "react";
 import Project from "./Project";
 import {
-//  webProjects,
+  webProjects,
   mobileProjects,
   terminalProjects,
   desktopProjects
 } from "../../data/projectInfo";
 import { Tabs, Tab } from "react-bootstrap";
-
-const webProjects = {};
-
-useEffect(() => {
-    fetch('https://5g624c23tc.execute-api.us-east-1.amazonaws.com/default/lewiscodes-projects-web')
-        .then(response => response.json())
-        .then(data => webProjects = data));
-}
 
 const Projects = () => {
   return (
